9ecaa56c45282d2ba030a4bf990185e2fb9932b6
Pietro <74447932+pietroepis@users.noreply.github.com>
2020-12-24 17:46:35 +0100

    Forse funziona

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 
 int get_blosum_index(char c)
 {
-	char* symbols = "ARNDCQEGHILKMFPSTWYV";
+	char* symbols = "ARNDCQEGHILKMFPSTWYVBZX";//"ARNDCQEGHILKMFPSTWYV";
+					 
 	int i;
 	for (i=0; i<strlen(symbols); i++)
 		if (toupper(c) == symbols[i])
@@ -13,10 +14,12 @@ int get_blosum_index(char c)
 
 void print_matrix(int** M, int h, int w)
 {
-	int j;
-	for (j = 0; j<w; j++)
-		printf("%d\t", M[h-1][j]);
-			
+	int j, i;
+	for (i = 0; i< h; i++ ){
+		for (j = 0; j<w; j++)
+			printf("%d\t", M[i][j]);
+		printf("\n");
+	}
 	printf("\n");
 }
 
@@ -37,31 +40,31 @@ int calc_alignment(char* s1, char* s2, int b)
 	
 	for (i = 0; i<l1; i++)
 		for (j = 0; j<b_width; j++)
-			M[i][j] = 0;
+			M[i][j] = INT_MIN;
 	
 	for (i=0; i<l1; i++) {
 		int initial_j = b - i;
-		int final_j = b_width;
-		
-		if (j < 0)
+		int final_j = initial_j + strlen(s2) + 1;
+		if(final_j>b_width) final_j = b_width;
+		if (initial_j < 0)
 		{
-			int diff = j * -1;
 			initial_j = 0;
-			final_j = b_width - diff;
 		}
-		
+		if(i==0) M[0][initial_j] = 0;
+		//printf("initial_j %d, final_j %d \n", initial_j, final_j);
 		for (j=initial_j; j<final_j; j++)
 		{						
-			if (i == 0 && j > 0)
+			if (i == 0 && j > initial_j)
 				M[i][j] = M[i][j-1] - 4;
-			if (i > 0 && j == 0)
+			if (i > 0 && j == initial_j)
 				M[i][j] = M[i-1][j+1] - 4;
-			if (i > 0 && j > 0)
-			{
-				M[i][j] = M[i-1][j] + blosum[get_blosum_index(s1[i-1])][get_blosum_index(s2[j+i-b])];
-				P[i][j] = '\\';
 			
-				if (M[i-1][j] - 4 > M[i][j])
+			if (i > 0 && j > initial_j)
+			{
+				M[i][j] = M[i-1][j] + blosum[get_blosum_index(s1[i-1])][get_blosum_index(s2[j+i-b-1])];
+				P[i][j] = '\\';
+				//printf("%d", blosum[get_blosum_index(s1[i-1])][get_blosum_index(s2[j+i-b-1])]);
+				if (j < b_width - 1 && M[i-1][j+1] - 4 > M[i][j])
 				{
 					M[i][j] = M[i-1][j+1] - 4;
 					P[i][j] = '|';
@@ -72,22 +75,26 @@ int calc_alignment(char* s1, char* s2, int b)
 					P[i][j] = '-';
 				}
 			}
+			//printf("%d %d %d", M[i][j], i, j);
+			//system("pause");
 		}
 	}
 	
-	print_matrix(M, l1, b_width);
-	
-	printf("ALIGNMENT: %d\n", M[l1-1][b_width-l1]);
-	return M[l1-1][b_width-1];
+/*	print_matrix(M, l1, b_width);
+	printf("ALIGNMENT: %d\n", M[l1-1][strlen(s2) - l1 + b + 1]);
+	printf("%d", strlen(s2) - l1 + b - 1);
+	system("pause");*/
+	return M[l1-1][strlen(s2) - (l1 - 1) + b];
 }
 
 int self_alignment(char* s1)
 {
-	int l1 = strlen(s1) + 1;
+	int l1 = strlen(s1);
 	int i, alignment = 0;
 	
-	for (i=0; i<l1; i++)
-		alignment += blosum[get_blosum_index(s1[i]), get_blosum_index(s1[i])];
+	for (i=0; i<l1; i++) {
+		alignment += blosum[get_blosum_index(s1[i])][get_blosum_index(s1[i])];
+	}
 		
 	return alignment;
 }
@@ -117,7 +124,10 @@ int main(int argc, char *argv[])
 		alignment = calc_alignment(s1, s2, b);
 		bound = self + (-4) * b;
 		b *= 2;
-	} while (b <= (strlen(s1) > strlen(s2) ? strlen(s1) : strlen(s2)) && bound > alignment);
+		//printf("bound = %d b = %d\n", bound, b);
+	} while (b/2 <= (strlen(s1) > strlen(s2) ? strlen(s1) : strlen(s2)) && bound >= alignment);
 	
-	//printf("%d", alignment);
+	printf("%d", alignment);
+	
+	return 0;
 }
