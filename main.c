4c379c862e8e88bd0bb4cd9963c9cdf40f4f44d7
Pietro <74447932+pietroepis@users.noreply.github.com>
2020-12-27 20:51:35 +0100

    Extra

--- main.c
+++ main.c
@@ -26,15 +26,16 @@ void print_matrix(char** M, int h, int w)
 	printf("\n");
 }
 
-int calc_alignment(char* s1, char* s2, int b)
+int calc_alignment(char* s1, char* s2, int b, int extra)
 {
 	int l1 = strlen(s1) + 1, l2 = strlen(s2) + 1;
 	int alignment = 0;
 	M = (int**) malloc(sizeof(int*) * l1);
 	P = (char**) malloc(sizeof(char*) * l1);
-	int b_width = 2 * b + 1;
-	
+	int b_width = 2 * b + extra;
 	int i, j;
+	int initial_j, final_j;
+	
 	for (i=0; i<l1; i++)
 	{
 		M[i] = (int*)malloc(sizeof(int) * b_width);
@@ -46,8 +47,8 @@ int calc_alignment(char* s1, char* s2, int b)
 			M[i][j] = INT_MIN;
 	
 	for (i=0; i<l1; i++) {
-		int initial_j = b - i;
-		int final_j = initial_j + strlen(s2) + 1;
+		initial_j = b_width / 2 - i;
+		final_j = initial_j + strlen(s2) + 1;
 		if(final_j>b_width) final_j = b_width;
 		if (initial_j < 0)
 		{
@@ -63,53 +64,51 @@ int calc_alignment(char* s1, char* s2, int b)
 		{						
 			if (i == 0 && j > initial_j)
 			{
-				M[i][j] = M[i][j-1] - 4;
+				M[i][j] = M[i][j - 1] - 4;
 				P[i][j] = '-';
 			}
 			if (i > 0 && j == initial_j)
 			{
-				M[i][j] = M[i-1][j+1] - 4;
+				M[i][j] = M[i - 1][j + 1] - 4;
 				P[i][j] = '|';
 			}
 			
 			if (i > 0 && j > initial_j)
 			{
-				M[i][j] = M[i-1][j] + blosum[get_blosum_index(s1[i-1])][get_blosum_index(s2[j+i-b-1])];
+				M[i][j] = M[i - 1][j] + blosum[get_blosum_index(s1[i - 1])][get_blosum_index(s2[j + i - (b_width / 2) - 1])];
 				P[i][j] = '\\';
 				
 				if (j < b_width - 1 && M[i-1][j+1] - 4 > M[i][j])
 				{
-					M[i][j] = M[i-1][j+1] - 4;
+					M[i][j] = M[i - 1][j + 1] - 4;
 					P[i][j] = '|';
 				}
-				if (M[i][j-1] - 4 > M[i][j])
+				if (M[i][j - 1] - 4 > M[i][j])
 				{
-					M[i][j] = M[i][j-1] - 4;
+					M[i][j] = M[i][j - 1] - 4;
 					P[i][j] = '-';
 				}
 			}
 		}
 	}
 	
-	return M[l1 - 1][strlen(s2) - (l1 - 1) + b];
+	return M[l1 - 1][strlen(s2) - (l1 - 1) + (b_width / 2)];
 }
 
-void create_new_strings(char* s1, char* s2, int b)
+void create_new_strings(char* s1, char* s2, char* out_s1, char* out_s2, int b, int extra)
 {
 	int l1 = strlen(s1) + 1, l2 = strlen(s2);
-	int b_width = 2 * b + 1;
-	char* out_s1 = malloc(l1 + l2 + 1);
-	char* out_s2 = malloc(l1 + l2 + 1);
+	int b_width = 2 * b + extra;
 	int c1 = 0, c2 = 0;
 	int i = l1 - 1;
-	int j = l2 - (l1 - 1) + b;
+	int j = l2 - (l1 - 1) + (b_width / 2);
 	
 	while(P[i][j] == '-' || P[i][j] == '|' || P[i][j] == '\\')
 	{
 		if (P[i][j] == '-')
 		{
 			out_s1[c1] = '-';
-			out_s2[c2] = s2[j + i - b - 1];
+			out_s2[c2] = s2[j + i - (b_width / 2) - 1];
 			j--;
 		} 
 		else if (P[i][j] == '|')
@@ -122,19 +121,13 @@ void create_new_strings(char* s1, char* s2, int b)
 		else
 		{
 			out_s1[c1] = s1[i - 1];
-			out_s2[c2] = s2[j + i - b - 1];
+			out_s2[c2] = s2[j + i - (b_width / 2) - 1];
 			i--;
 		}
 		
 		c1++;
 		c2++;
 	}
-	
-	printf("%s\n", s1);
-	printf("%s\n\n\n", s2);
-	
-	printf("%s\n", out_s1);
-	printf("%s\n", out_s2);
 }
 
 int self_alignment(char* s1)
@@ -151,33 +144,51 @@ int self_alignment(char* s1)
 
 int main(int argc, char *argv[])
 {
-	FILE *fp;
-	int l1, l2;
+	FILE *fin, *fout;
+	int l1, l2, b, extra;
+	int self, bound, alignment;
+	char* s1;
+	char* s2;
+	char* out_s1;
+	char* out_s2;
 	
-	fp = fopen("input.txt", "r");
+	fin = fopen("input.txt", "r");
 	
-	fscanf(fp, "%d", &l1);
-	char* s1 = malloc(sizeof(char) * l1);
-	fscanf(fp, "%s", s1);
+	fscanf(fin, "%d", &l1);
+	s1 = malloc(sizeof(char) * l1);
+	fscanf(fin, "%s", s1);
 	
-	fscanf(fp, "%d", &l2);
-	char* s2 = malloc(sizeof(char) * l2);
-	fscanf(fp, "%s", s2);
+	fscanf(fin, "%d", &l2);
+	s2 = malloc(sizeof(char) * l2);
+	fscanf(fin, "%s", s2);
 	
-	int self = self_alignment(s1);
-	int b = abs(strlen(s1) - strlen(s2));
+	fclose(fin);
+	
+	self = self_alignment(s1);
+	b = abs(strlen(s1) - strlen(s2));
+	extra = 1;	
 	if (b == 0)
 		b += 1;
 	
-	int bound, alignment;
 	do {
-		alignment = calc_alignment(s1, s2, b);
+		alignment = calc_alignment(s1, s2, b, extra);
 		bound = self + (-4) * b;
-		b *= 2;
-	} while (b/2 <= (strlen(s1) > strlen(s2) ? strlen(s1) : strlen(s2)) && bound >= alignment);
+		extra *= 2;
+	} while ((b + 2 * extra) <= (strlen(s1) > strlen(s2) ? strlen(s1) : strlen(s2)) && bound >= alignment);
 	
-	printf("%d\n\n", alignment);
-	create_new_strings(s1, s2, b/2);
+	out_s1 = malloc(l1 + l2 + 1);
+	out_s2 = malloc(l1 + l2 + 1);
+	create_new_strings(s1, s2, out_s1, out_s2, b/2, extra);
+	
+	printf("ALIGNMENT: %d\n", alignment);
+	printf("S1: %s\nS2: %s\n", s1, s2);
+	printf("OUT_S1: %s\nOUT_S2: %s\n", out_s1, out_s2);
+	
+	fout = fopen("output.txt", "w");
+	fprintf(fout, "ALIGNMENT: %d\n\n", alignment);
+	fprintf(fout, "%s\n", out_s1);
+	fprintf(fout, "%s", out_s2);
+	fclose(fout);	
 	
 	return 0;
 }
